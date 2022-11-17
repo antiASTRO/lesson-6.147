4b4292d3e6ffd5ecaa7644d8ad6785c4227c7ccb
firdavs <fxcker@gmail.com>
2022-11-17 21:31:33 +0500

    functions 2

---
+++ typescript.ts
@@ -0,0 +1,19 @@
+// function hello(a: number, b: number) {
+//     let c: number = (a - b) /2;
+//     console.log(c + 12.78);    
+// }
+
+// hello(12,14)
+
+// function bye (x: string, i: number) {
+//     let ch: string = x [i] 
+//     console.log('  char at ' + i + ' = ' + ch);   
+// }
+
+// bye('salom', 11)
+
+function hii (firstname: string, lastname: string ) {
+    console.log(firstname.length + lastname.length);
+}
+
+hii('Jalol', 'Imomaddinov')
